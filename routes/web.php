f2fa26fabf6b95d13d4b3dc5e7e4fa766bb6e597
kapil707 <kapil707sharma@gmail.com>
2023-10-27 12:45:45 +0530

    add

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('login', function () {
+    return view('vp-admin/login/index');
+});
 Route::get('admin/', function () {
     return view('vp-admin/login/index');
 });
