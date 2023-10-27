3d5fdce9518f2270936b6e0d1ba5e74c6437f94b
kapil707 <kapil707sharma@gmail.com>
2023-10-27 12:18:16 +0530

    add

--- resources/views/vp-admin/manage_master/meter.blade.php
+++ resources/views/vp-admin/manage_master/meter.blade.php
@@ -16,10 +16,19 @@
 									Sno.
 								</th>
 								<th>
-									Title
+									user_code
 								</th>
 								<th>
-									Update Time
+									user_altercode
+								</th>
+								<th>
+									image
+								</th>
+								<th>
+									meter_text
+								</th>
+								<th>
+								data/time
 								</th>
 								<th>
 									Action
@@ -32,17 +41,25 @@
 						foreach ($result as $row)
 						{
 							?>
-							<tr id="row_<?= $row->id; ?>" <?php if($row->status==0){ ?> class="text-warning" <?php } ?>>
+							<tr id="row_<?= $row->id; ?>">
 								<td>
 									<?= $i++; ?>
 								</td>
 								<td>
-									<?= ($row->title); ?>
+									<?= ($row->user_code); ?>
 								</td>
 								<td>
-									<?php
-									echo ($row->updated_at);
-									?>
+									<?= ($row->user_altercode); ?>
+								</td>
+								<td>
+									<?= ($row->image); ?>
+								</td>
+								<td>
+									<?= ($row->meter_text); ?>
+								</td>
+								<td>
+									<?= ($row->date);?>
+									<?= ($row->time);?>
 								</td>
 								<td class="text-right">
 									<div class="btn-group">
