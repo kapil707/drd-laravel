f26bec53a59a16f2aab190219ea52d4d05adbc31
kapil707 <kapil707sharma@gmail.com>
2023-10-27 13:33:48 +0530

    add

--- routes/web.php
+++ routes/web.php
@@ -27,16 +27,16 @@ Route::get('vp-admin/',[AdminController::class,'login']);
 Route::get('adminlogout/',[AdminController::class,'logout']);
 
 Route::POST('vp-admin/admin_submit', [AdminController::class,'admin_submit']);
-Route::get('vp-admin/dashboard', [AdminController::class,'dashboard']);
+Route::get('vp-admin/dashboard', [AdminController::class,'dashboard'])->middleware('Adminauth');
 
 /*******Profile_Management_Controller *************/
-Route::get('vp-admin/profile_management', [Profile_Management_Controller::class,'index']);
+Route::get('vp-admin/profile_management', [Profile_Management_Controller::class,'index'])->middleware('Adminauth');
 
 
 /*******manage_page*************/
-Route::any('vp-admin/manage_page/',[Manage_page_Controller::class,'index']);
-Route::any('vp-admin/manage_page/{var1}', [Manage_page_Controller::class,'index']);
-Route::any('vp-admin/manage_page/{var1}/{var2}', [Manage_page_Controller::class,'index']);
+Route::any('vp-admin/manage_page/',[Manage_page_Controller::class,'index'])->middleware('Adminauth');
+Route::any('vp-admin/manage_page/{var1}', [Manage_page_Controller::class,'index'])->middleware('Adminauth');
+Route::any('vp-admin/manage_page/{var1}/{var2}', [Manage_page_Controller::class,'index'])->middleware('Adminauth');
 
 /*******manage_master*************/
 Route::any('vp-admin/manage_master/',[Manage_master_Controller::class,'index'])->middleware('Adminauth');
