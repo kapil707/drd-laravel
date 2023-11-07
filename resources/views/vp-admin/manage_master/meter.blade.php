e5688b7162b71c573fa9ded53f4d5adb322b8884
Kapil <kapil@Kapils-MacBook-Air.local>
2023-11-07 15:28:25 +0530

    add

--- resources/views/vp-admin/manage_master/meter.blade.php
+++ resources/views/vp-admin/manage_master/meter.blade.php
@@ -52,8 +52,8 @@
 									<?= ($row->user_altercode); ?>
 								</td>
 								<td>
-									<a href="https://drdweb.co.in/upload_drd_master/meter_photo/<?= ($row->date);?>/<?= ($row->image1); ?>" target="_blank">
-									<img src="https://drdweb.co.in/upload_drd_master/meter_photo/<?= ($row->date);?>/<?= ($row->image1); ?>" width=100></a>
+									<a href="https://drdweb.co.in/upload_drd_master/meter_photo/<?= ($row->date);?>/<?= ($row->image); ?>" target="_blank">
+									<img src="https://drdweb.co.in/upload_drd_master/meter_photo/<?= ($row->date);?>/<?= ($row->image); ?>" width=100></a>
 								</td>
 								<td>
 									<?= ($row->message); ?>
