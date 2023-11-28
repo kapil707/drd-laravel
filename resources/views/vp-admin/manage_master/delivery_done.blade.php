65dad51982021145280ab9a3b6eecc49db3ff5c8
Kapil <kapil@Kapils-MacBook-Air.local>
2023-11-28 12:38:01 +0530

    add

--- resources/views/vp-admin/manage_master/delivery_done.blade.php
+++ resources/views/vp-admin/manage_master/delivery_done.blade.php
@@ -8,7 +8,7 @@
     <div class="col-xs-12">
 		<div class="ibox float-e-margins">
 			<div class="ibox-content">
-				<form method="post">
+				<form method="get">
 					@csrf
 					<div class="row">
 						<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
